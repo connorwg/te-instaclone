8bf1a834e6bff714f7837a65e6a5dc4c4b197bfe
Connor Gobble <cwgobble@yahoo.com>
2022-08-08 13:32:18 -0400

    Updated DB Scheme + Test data

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,25 @@
 BEGIN TRANSACTION;
 
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('user2','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('user3','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+
+INSERT INTO pictures (user_id, s3_link, description, time)  VALUES (1, 'https://funnypicture.com', 'this is a funny pic', NOW());
+INSERT INTO pictures (user_id, s3_link, description, time)  VALUES (2, 'hahahahahaha', 'WOW', NOW());
+INSERT INTO pictures (user_id, s3_link, description, time)  VALUES (3, 'BOOMER FOOD PICTURE', 'yum xD', NOW());
+INSERT INTO pictures (user_id, s3_link, description, time)  VALUES (1, 'blurry pic', 'i am artsy :p', NOW());
+
+INSERT INTO comments (comment, user_id, picture_id) VALUES ('wow this is cool' , 3, 2);
+INSERT INTO comments (comment, user_id, picture_id) VALUES ('nice hahahahahah wowowowow' , 3, 3);
+INSERT INTO comments (comment, user_id, picture_id) VALUES ('wow that looks good gerry !!!!! -love mom', 1, 4);
+INSERT INTO comments (comment, user_id, picture_id) VALUES ('art school is rlly paying off huh ????' , 2, 5);
+
+INSERT INTO liked_by_user VALUES (1, 2, TRUE);
+INSERT INTO liked_by_user VALUES (2, 2, TRUE);
+INSERT INTO liked_by_user VALUES (3, 2, TRUE);
+INSERT INTO liked_by_user VALUES (4, 2, TRUE);
+
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,87 @@
-BEGIN TRANSACTION;
+BEGIN;
+DROP TABLE IF EXISTS is_favorited, liked_by_user,comments, pictures,users;
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id serial NOT NULL,
+    username character varying(50) NOT NULL,
+    password_hash character varying(200) NOT NULL,
+    role character varying(50) NOT NULL,
+    PRIMARY KEY (user_id)
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE IF NOT EXISTS public.pictures
+(
+    picture_id serial NOT NULL,
+    user_id integer NOT NULL,
+    s3_link character varying NOT NULL,
+    description character varying(200),
+    "time" timestamp with time zone NOT NULL,
+    PRIMARY KEY (picture_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.comments
+(
+    comment_id serial NOT NULL,
+    comment character varying(200) NOT NULL,
+    user_id integer NOT NULL,
+    picture_id integer,
+    PRIMARY KEY (picture_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.liked_by_user
+(
+    user_id integer NOT NULL,
+    picture_id integer NOT NULL,
+    liked_by_user boolean NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.is_favorited
+(
+    picture_id integer NOT NULL,
+    user_id integer NOT NULL,
+    is_favorited boolean NOT NULL
+);
+
+ALTER TABLE public.pictures
+    ADD FOREIGN KEY (user_id)
+    REFERENCES public.users (user_id)
+    NOT VALID;
+
+
+ALTER TABLE public.liked_by_user
+    ADD FOREIGN KEY (user_id)
+    REFERENCES public.users (user_id)
+    NOT VALID;
+
+
+ALTER TABLE public.liked_by_user
+    ADD FOREIGN KEY (picture_id)
+    REFERENCES public.pictures (picture_id)
+    NOT VALID;
+
+
+ALTER TABLE public.is_favorited
+    ADD FOREIGN KEY (picture_id)
+    REFERENCES public.pictures (picture_id)
+    NOT VALID;
+
+
+ALTER TABLE public.is_favorited
+    ADD FOREIGN KEY (user_id)
+    REFERENCES public.users (user_id)
+    NOT VALID;
+
+
+ALTER TABLE public.comments
+    ADD FOREIGN KEY (picture_id)
+    REFERENCES public.pictures (picture_id)
+    NOT VALID;
+
+
+ALTER TABLE public.comments
+    ADD FOREIGN KEY (user_id)
+    REFERENCES public.users (user_id)
+    NOT VALID;
+
+END;
