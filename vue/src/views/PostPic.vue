fd7c89e775b4ad2c2036ec911416fe21c6508b99
Prachi Tyagi <prachityagi113@gmail.com>
2022-08-11 16:08:07 -0400

    Bsic page to post pic, more functionalities to come

---
+++ vue/src/views/PostPic.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>
+          <Header></Header>
+        </h1>
+        <input type="file" accept="image/*" @change="onChange" />
+        <div>
+            <img id="preview" v-if="item.imageUrl" :src="item.imageUrl" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+ name: "postpic",
+  components: {
+    Header,   
+ },
+   data() {
+    return {
+      item:{
+          //...
+          image : null,
+          imageUrl: null
+      }
+    }
+  },
+    methods: {
+    preview(event) {
+      const file = event.target.files[0]
+      this.image = file
+      this.item.imageUrl = URL.createObjectURL(file)
+    }
+  }
+}
+</script>
+<style>
+    #preview {
+        width: 200px;
+        height: 200px;
+    }
+</style>
